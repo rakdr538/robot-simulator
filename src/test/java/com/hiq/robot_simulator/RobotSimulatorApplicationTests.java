d5872706393e52addfed2ef4daf7cbe087084041
Rakesh D <rakdr538@gmail.com>
2025-01-17 11:49:36 +0100

    Committing initial project.

---
+++ src/test/java/com/hiq/robot_simulator/RobotSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiq.robot_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
